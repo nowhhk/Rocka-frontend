5cd5358e04da222bf7b8a4b511333632379adc80
권미란 <gwon.rran@gmail.com>
2020-05-26 20:29:27 +0900

    SignIn Page, SignUp page

--- src/Pages/SignIn/SignIn.js
+++ src/Pages/SignIn/SignIn.js
@@ -1,11 +1,11 @@
-import React, {Component} from "react";
+import React, { Component } from "react";
 import "./SignIn.scss";
 
 class SignIn extends Component {
-    render(){
-        return(
-            <div class="SignIn">
-                <div class="SignInBox">
+    render() {
+        return (
+            <div className="SignIn">
+                <div className="SignInBox">
                     <h1>Login</h1>
                     <div className="SignInText">
                         회원으로 가입하시면 10%웰컴쿠폰을 드립니다.<br /><br />
@@ -16,21 +16,21 @@ class SignIn extends Component {
                         <tbody>
                             <tr>
                                 <th>아이디</th>
-                                <td><input type="" /></td>
+                                <td><input type="text" /></td>
                             </tr>
                             <tr>
                                 <th>비밀번호</th>
-                                <td><input type="" /></td>
+                                <td><input type="text" /></td>
                             </tr>
                         </tbody>
                     </table>
                     <div className="SignInBtn"><span>로그인</span></div>
                     <div className="SignInEtc">
-                        <div class="searchAccount">
+                        <div className="searchAccount">
                             <sapn className="searchID">아이디찾기</sapn>
                             <sapn>비밀번호찾기</sapn>
                         </div>
-                        <div class="SignUp">회원가입</div>
+                        <div className="LoginSignUp">회원가입</div>
                     </div>
                 </div>
             </div>
